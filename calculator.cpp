45d79582ce0226b1f0f91889be7e0c294945301b
Bernadette Angelice Novena Octaviani <belajarcodingangelicenovenaoct@gmail.com>
2025-12-25 08:22:38 +0700

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float n1, n2;
+    cout << "Masukkan dua angka: ";
+    cin >> n1 >> n2;
+    cout << "Hasil Pertambahan: " << n1 + n2 << endl;
+    return 0;
+}
